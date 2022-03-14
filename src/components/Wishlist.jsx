4a4620c5621502e71abc330d2189715194e3d566
Mangesh2042 <mangesh2042@gmail.com>
2022-03-14 21:10:25 +0530

    hide button and input after limit

--- src/components/Wishlist.jsx
+++ src/components/Wishlist.jsx
@@ -2,9 +2,7 @@ import React from "react";
  const Wishlist=()=>{
     const [query,setQuery]=React.useState("");
     const [tasks,setTasks]=React.useState([]);
-
-   const handleChange=(event)=>{ setQuery(event.target.value)}
-  
+    const handleChange=(event)=>{ setQuery(event.target.value)}
    const AddtoList=()=>
    {
        const obj={
@@ -13,21 +11,20 @@ import React from "react";
         if(tasks.length==3)
         {
             alert("You cannot add more than 3 items to wishlist")
+            document.getElementById("Addbtn").style.display = "none"
+            document.getElementById("input").style.display = "none"
         }
         else{
             let list=[...tasks,obj]
             setTasks(list)
         }
-     
    }
    return(
         <>
             <h1>Wishlist</h1>
-            <input type="text" placeholder="Enter Wishlist Name" onChange={handleChange} value={query}/>
-            <button onClick={AddtoList}>ADD</button>
-            {  
-
-                
+            <input type="text" id="input" placeholder="Enter Wishlist Name" onChange={handleChange} value={query}/>
+            <button onClick={AddtoList} id="Addbtn">ADD</button>
+            {     
                 tasks.map(({names})=>{ return <div>{names}</div>})
             }
         </>
